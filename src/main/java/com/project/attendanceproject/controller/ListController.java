9f10b41f3ec1276553f9e1711a963315c09c32f9
Emmanuel <immanuel.raj18@gmail.com>
2019-04-10 09:27:49 +0530

    Initial commit in develop branch

--- src/main/java/com/project/attendanceproject/controller/ListController.java
+++ src/main/java/com/project/attendanceproject/controller/ListController.java
@@ -1,15 +1,51 @@
 package com.project.attendanceproject.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
-//@Controller
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.attendanceproject.dto.AttendanceDTO;
+import com.project.attendanceproject.interfaces.AttendanceRepository;
+
+@RestController
 public class ListController 
 {
+	
+	
+	  @Autowired
+     AttendanceRepository attendanceRepo;
+	  
+	  
+	//logger
+	  private static final Log logger = LogFactory.getLog(ListController.class);
+	  
     /**
-	@GetMapping("/secured/list")
-	public String listFormBackingObject()
+     * 
+     * @return
+     */
+	//@GetMapping("/secured/list")
+	public List<AttendanceDTO> listFormBackingObject()
 	{
-		
-	}*/
+		logger.info("listFormBackingObject");
+		return attendanceRepo.findAll();
+	}
+	
+	
+	@GetMapping("/secured/list")
+	public  List<AttendanceDTO>  findById(HttpServletRequest request)
+	{
+		logger.info(request.getRemoteUser());
+		return attendanceRepo.findByUserName(request.getRemoteUser());
+	}
+	
+	
+	
+	
+	
 }
--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -8,60 +8,9 @@
  * while blur of confirmPassword the  
  */
 
+console.log(window);
 
-
-function pr_checkPassword()
+function hi()
 {
-   console.log('checkPassword')
-   
-   alert('checkPassword')
-   
-   if((document.getElementById('confirmPassword').value)===(document.getElementById('password').value) && (!document.getElementById('confirmPassword').value)==(document.getElementById('password').value))
-	   {
-	         
-	   }
-   
-   else
-	   {
-	       alert('password not match ')
-	       
-	       
-	   }
-}
-
-
-
-
-function co_isEmailAddress(elem)
-{
-	
-	var x= pr_checkPassword();
-	
-	
-	alert(x);
-}
-
-
-
-function myFunction()
-{
-	  
-	  
-	  console.log('myFn');
-	  alert('page is loaded');
-}
-
-function pr_username()
-{
-	  
-	  console.log('un')
-	  alert('username is loaded');
-}
-
-
-
-function myfn()
-{
-	
-   alert('myfn');
+  alert('hi');	
 }
